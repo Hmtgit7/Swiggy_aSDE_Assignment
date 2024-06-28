8a37edd2285cde55f7c2670c21e8e802dc956e74
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-06-28 16:21:19 +0530

    💬 Adding Comments 💬

--- Dice.java
+++ Dice.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 
+// THIS CLASS REPRESENTS A DICE WITH 6 SIDES. IT HAS A METHOD ROLL() THAT RETURNS A RANDOM NUMBER BETWEEN 1 AND 6.
 
 public class Dice {
     private static final int sides=6;
@@ -9,6 +10,7 @@ public class Dice {
         this.random = new Random(); 
     }
 
+    // ROLL METHOD THAT RETURNS A RANDOM NUMBER BETWEEN 1 AND 6.
     public int roll(){
         return random.nextInt(sides) + 1;
     }
